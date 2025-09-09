4c93166a059a1e3d74d0fd64e4af05863df39c94
Pablo Joaquim <pablojoaquim@gmail.com>
2025-09-08 21:03:10 -0300

    The file extractor and parser is ready. All the blocks are read and verified. The next step is to get the data field from each block.

--- README.md
+++ README.md
@@ -1,1 +1,91 @@
 # Asentria-TechnicalChallenge
+
+## Programming Exercise
+
+NOTE: ALL INFORMATION REQUIRED TO COMPLETE THE TASK IS INCLUDED IN THIS README FILE.
+
+The INTDATA file contains an hex-ASCII dump of binary data, with some header
+information for each block. This file was produced by extracting the binary data
+from a data collection unit running an Intel CPU, and saving it in human-readable
+format. The task is to write a recovery program that operates on the hex-ASCII
+representation of the binary data and recovers the data correctly.
+
+Here is a 'c' structure definition which shows, in the data block, the
+positions of the items in the block:
+
+struct flashdata
+    {
+    uint16 filename_id;                     // number for file
+    uint32 sequence_in_file;                // file sequence number
+    uint16 number_records;                  // recs in block
+    uint16 data_length;                     // data length
+    uint16 flags;                           // unused presently
+    unsigned char compression_type;         // unused presently
+    unsigned char spare;                    // unused presently
+    unsigned char data[2048-14];            // the data itself
+    };
+
+The blocks need to have the binary data extracted from them.  Then check the
+block.  If the filename_id is not 0002 then discard the block.  If the block
+has obvious errors (data_length>2048-14) then discard.  Otherwise use the
+data_length, number_records and data fields to extract the data records.
+
+
+Record structure:
+
+<length><flags><len2> <data.......>
+
+Length is the length of the record and the 3-byte header.  The other two
+bytes of the header can be ignored for this purpose.  The data is the
+compressed record data.
+
+The data is compressed in the following manner:
+
+All input data is ASCII, character <128.
+Any series of spaces is first compressed into a single character which is
+0x80 + the number of spaces.  For example, a series of 5 spaces is compressed
+into a single 0x85 character.
+
+Then a second compression method is employed. A table is used to compress
+characters into nibbles.  If a character is not in the table then three
+nibbles, first F then the two nibbles of the character are used.
+
+The table is shown below:
+
+Character           Nibble
+0                   0
+1                   1
+2                   2
+3                   3
+4                   4
+5                   5
+6                   6
+7                   7
+8                   8
+9                   9
+space               A       (single space character)
+:                   B
+/                   C
+carraige return     D
+linefeed            E
+literal character   F
+
+First the low nibble of a byte is used, then the high nibble.
+If the last nibble is not used, it should be padded with F to ensure that
+it is not used in decompression.
+
+
+For example,
+
+0123456 C<cr><lf> is compressed as:
+10 32 54 A6 4F D3 FE
+
+
+01          23 is compressed as:
+10 af 28 f3
+
+The extraction consists of undoing the nibble compression, then undoing
+any space compressions and writing the data to a file.
+
+This data has been previously recovered and will be used to check the
+correctness of the data extraction.
---
+++ main.c
@@ -0,0 +1,217 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+
+typedef struct flashdata_Tag
+{
+    uint16_t filename_id;                     // number for file
+    uint32_t sequence_in_file;                // file sequence number
+    uint16_t number_records;                  // recs in block
+    uint16_t data_length;                     // data length
+    uint16_t flags;                           // unused presently
+    unsigned char compression_type;           // unused presently
+    unsigned char spare;                      // unused presently
+    unsigned char data[2048-14];              // the data itself
+}flashdata_T;
+
+typedef enum cs_parser_tag
+{
+    CS_LOOKING_FOR_BLOCK_START,
+    CS_LOOKING_FOR_BLOCK_NUMBER,
+    CS_READ_METADATA,
+    CS_READ_DATA_BLOCK
+}cs_parser_t;
+
+int main() 
+{
+    // Control variables
+    FILE *file;                 // file* necessary for file operations
+    char line[50];              // Buffer to store each line
+    unsigned int bytes[14];     // Buffer to store the flashdata
+    int block_seq = 0;          // Check the sequence of block in the file
+    int block_counter = 0;      // block counter for statistics
+    int block_ok_counter = 0;   // block ok counter for statistics
+    int block_err_counter = 0;  // block error counter for statistics
+    int line_counter = 0;       // line counter for statistics
+    int block_number;           // The parsed block number in the label
+    int block_number_aux;       // The parsed block number
+    flashdata_T flashdata;      // The flashdata information from each block
+    cs_parser_t cs_parser = CS_LOOKING_FOR_BLOCK_START;  // The state variable for the parser
+    
+    // Open the file for reading operations
+    // The filename is hardcoded. An improvement could be 
+    // to receive the name as an args
+    file = fopen("INTDATA", "r");
+    if (file == NULL) 
+    {
+        perror("Error opening file");
+        return -1;
+    }
+
+    // Read every line through the file
+    while (fgets(line, sizeof(line), file) != NULL) 
+    {
+        // Remove trailing newline character if present
+        line[strcspn(line, "\n")] = 0; 
+        
+        // Increment the line counter
+        line_counter++;
+
+        // Print the line while parsing
+        printf("Parsed line %d: \t\t %s\n", line_counter, line);
+
+        // The parser
+        switch(cs_parser)
+        {
+            case CS_LOOKING_FOR_BLOCK_START:
+                // Check for the "BLK" start block identifier
+                if(0 == strncmp(line,"BLK", 3))
+                {
+                    // Count the number of block
+                    // could be a good block or a corrupted one
+                    block_counter++;
+
+                    // Read the block number in the block start label
+                    sscanf(&line[4], "%d", &block_number);
+                    //printf("block number is: %d\n", block_number);
+
+                    // Move to the next state of the parser
+                    cs_parser = CS_LOOKING_FOR_BLOCK_NUMBER;
+                }
+                break;
+
+            case CS_LOOKING_FOR_BLOCK_NUMBER:
+                sscanf(&line[0], "%d", &block_number_aux);
+                //printf("block number really is: %d\n", block_number_aux);
+                if (block_number_aux == block_number)
+                {
+                    cs_parser = CS_READ_METADATA;
+                    // // If this is the firts block store that block number
+                    // if (block_seq == 0)
+                    // {
+                    //     block_seq = block_number;
+                    //     cs_parser = CS_READ_METADATA;
+                    // }
+                    // else
+                    // {   
+                    //     if ((block_seq+1) != block_number)
+                    //     {
+                    //         // ERROR detected! Discard the whole block!                    
+                    //         block_err_counter++;
+                    //         printf("block_seq: %d", block_seq);
+                    //         printf("Error: Invalid Block sequence!\n");
+                    //         cs_parser = CS_LOOKING_FOR_BLOCK_START;
+                    //         return -1;                           
+                    //     }
+                    //     else
+                    //     {
+                    //         block_seq++;
+                    //         cs_parser = CS_READ_METADATA;
+                    //     }
+                    // }
+                }
+                else
+                {
+                    // ERROR detected! Discard the whole block!                    
+                    block_err_counter++;
+                    printf("Error: Invalid Block number!\n");
+                    cs_parser = CS_LOOKING_FOR_BLOCK_START;
+                }
+                break;
+
+            case CS_READ_METADATA:
+                int fields_read = sscanf(line, "%x %x %x %x %x %x %x %x %x %x %x %x %x %x",
+                                    &bytes[0], &bytes[1], &bytes[2], &bytes[3], &bytes[4], &bytes[5], &bytes[6], &bytes[7], &bytes[8],
+                                    &bytes[9], &bytes[10], &bytes[11], &bytes[12], &bytes[13]);
+                                    
+                if (fields_read == 14) 
+                {
+                    // The data is little-endian, so we combine the bytes in reverse order.
+                    flashdata.filename_id = (uint16_t)bytes[0] | ((uint16_t)bytes[1] << 8);
+                    flashdata.sequence_in_file = (uint32_t)bytes[2] | ((uint32_t)bytes[3] << 8) | ((uint32_t)bytes[4] << 16) | ((uint32_t)bytes[5] << 24);
+                    flashdata.number_records = (uint16_t)bytes[6] | ((uint16_t)bytes[7] << 8);
+                    flashdata.data_length = (uint16_t)bytes[8] | ((uint16_t)bytes[9] << 8);
+                    flashdata.flags = (uint16_t)bytes[10] | ((uint16_t)bytes[11] << 8);
+                    flashdata.compression_type = (uint8_t)bytes[12];
+                    flashdata.spare = (uint8_t)bytes[13];
+                    
+                    printf("Filename ID: %u (0x%X)\n", flashdata.filename_id, flashdata.filename_id);
+                    printf("Sequence in File: %u (0x%X)\n", flashdata.sequence_in_file, flashdata.sequence_in_file);
+                    printf("Number of Records: %u (0x%X)\n", flashdata.number_records, flashdata.number_records);
+                    printf("Data Length: %u (0x%X)\n", flashdata.data_length, flashdata.data_length);
+                    printf("Flags: %u (0x%X)\n", flashdata.flags, flashdata.flags);
+                    printf("Compression Type: %u (0x%X)\n", flashdata.compression_type, flashdata.compression_type);
+                    printf("Spare: %u (0x%X)\n", flashdata.spare, flashdata.spare);
+
+                    if (flashdata.filename_id != 0x02)
+                    {
+                        // ERROR detected! Discard the whole block!
+                        block_err_counter++;
+                        printf("Error: Invalid filename_id.\n");
+                        cs_parser = CS_LOOKING_FOR_BLOCK_START;
+                    }
+                    else if (flashdata.data_length > (2048-14))
+                    {
+                        // ERROR detected! Discard the whole block!
+                        block_err_counter++;
+                        printf("Error: Invalid data_length.\n");
+                        cs_parser = CS_LOOKING_FOR_BLOCK_START;
+                    }
+                    else
+                    {
+                        if (block_seq == 0)
+                        {
+                            block_seq = flashdata.sequence_in_file;
+                            cs_parser = CS_READ_DATA_BLOCK;
+                        }
+                        else
+                        {
+                            if (flashdata.sequence_in_file > block_seq )
+                            {
+                                block_seq = flashdata.sequence_in_file;
+                                cs_parser = CS_READ_DATA_BLOCK;
+                            }
+                            else
+                            {
+                                // ERROR detected! Discard the whole block!                    
+                                block_err_counter++;
+                                printf("Error: Invalid Block sequence!\n");
+                                cs_parser = CS_LOOKING_FOR_BLOCK_START;
+                            } 
+                        }
+                    }
+                } 
+                else 
+                {
+                    // ERROR detected! Discard the whole block!
+                    block_err_counter++;
+                    printf("Error: Could not read all fields. Read %d fields instead of 7.\n", fields_read);
+                    cs_parser = CS_LOOKING_FOR_BLOCK_START;                    
+                }
+                break;     
+
+            case CS_READ_DATA_BLOCK:
+
+                break;   
+
+            default:
+                break;
+        }
+
+
+        // More complex parsing logic would go here, e.g., using sscanf or strtok
+        // int id;
+        // char name[50];
+        // if (sscanf(line, "%d,%s", &id, name) == 2) {
+        //     printf("ID: %d, Name: %s\n", id, name);
+        // }
+    }
+
+    fclose(file);
+
+    printf("Number of blocks: %d\n", block_counter);
+    printf("Number of blocks ok: %d\n", block_ok_counter);
+    printf("Number of blocks error: %d\n", block_err_counter);
+    return 0;
+}
